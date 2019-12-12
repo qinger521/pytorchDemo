6d86b85a52bd18462dd925a806a579d964e580c0
bigmonkey521 <1004363192@qq.com>
2019-12-12 15:39:20 +0800

    初始化

--- LanguageModel/LanguageModelDemo.py
+++ LanguageModel/LanguageModelDemo.py
@@ -4,7 +4,12 @@ import torch
 import numpy as np
 import random
 
-#w为了保证实验结果可以复现，经常将各种random seed固定在某一个值
+# w为了保证实验结果可以复现，经常将各种random seed固定在某一个值
 random.seed(53113)
 np.random.seed(53113)
 torch.manual_seed(53113)
+
+BATCH_SIZE = 32
+# EMBEDDING_SIZE = 650 由于是在本机训练，所以可将EMBEDDING_SIZE设置的小一些
+EMBEDDING_SIZE = 100
+MAX_VOCAB_SIZE = 50000
