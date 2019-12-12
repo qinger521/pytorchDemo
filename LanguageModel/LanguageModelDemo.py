a51e9410c17bfc91c55f86c987739b95b09fa3d9
qinger521 <15610319606@163.com>
2019-12-12 16:50:40 +0800

    初始化

--- LanguageModel/LanguageModelDemo.py
+++ LanguageModel/LanguageModelDemo.py
@@ -13,3 +13,17 @@ BATCH_SIZE = 32
 # EMBEDDING_SIZE = 650 由于是在本机训练，所以可将EMBEDDING_SIZE设置的小一些
 EMBEDDING_SIZE = 100
 MAX_VOCAB_SIZE = 50000
+
+# 使用text8作为训练数据，pytorch一个重要的概念是Field，它决定了你的数据如何处理
+TEXT = torchtext.data.Field(lower=True)
+# 构建数据集
+train,val,test = torchtext.datasets.LanguageModelingDataset.splits(path="../text8"
+                                                  ,train="text8.train.txt",validation="text8.dev.txt"
+                                                  ,test="text8.test.txt",text_field=TEXT)
+TEXT.build_vocab(train,max_size=MAX_VOCAB_SIZE)
+# print(TEXT.vocab.itos[:10])
+train_iter,val_iter,test_iter = torchtext.data.BPTTIterator.splits((train,val,test),batch_size=BATCH_SIZE
+                                                                   ,device="cpu",bptt_len=50,repeat=False,shuffle=True)
+# it = iter(train_iter)
+# batch = next(it)
+# print(batch)
