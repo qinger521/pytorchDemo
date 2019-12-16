59aa0bc48c77271d25a9cd3bbaf67a4cb1247c7e
qinger521 <15610319606@163.com>
2019-12-16 15:16:52 +0800

    初始化

--- LanguageModel/LanguageModelDemo.py
+++ LanguageModel/LanguageModelDemo.py
@@ -1,4 +1,5 @@
 import torchtext
+import torch.nn as nn
 from torchtext.vocab import Vectors
 import torch
 import numpy as np
@@ -12,6 +13,7 @@ torch.manual_seed(53113)
 BATCH_SIZE = 32
 # EMBEDDING_SIZE = 650 由于是在本机训练，所以可将EMBEDDING_SIZE设置的小一些
 EMBEDDING_SIZE = 100
+HIDDEN_SIZE = 100
 MAX_VOCAB_SIZE = 50000
 
 # 使用text8作为训练数据，pytorch一个重要的概念是Field，它决定了你的数据如何处理
@@ -27,3 +29,60 @@ train_iter,val_iter,test_iter = torchtext.data.BPTTIterator.splits((train,val,te
 # it = iter(train_iter)
 # batch = next(it)
 # print(batch)
+class RNNModel(nn.Module):
+    def __init__(self,vocab_size,embed_size,hidden_size):
+        super(RNNModel,self).__init__()
+        self.hidden_size = HIDDEN_SIZE
+        self.embed = nn.Embedding(vocab_size,embed_size)
+        self.lstm = nn.LSTM(embed_size,hidden_size)
+        self.linear = nn.Linear(hidden_size,vocab_size)
+
+    def forward(self,text,hidden):
+        emb = self.embed(text)
+        output,hidden = self.lstm(emb,hidden)
+        out_vocab = self.linear(output.view(-1,output.shape[2]))
+        out_vocab = out_vocab.view(output.size(0),output.size(1),out_vocab.size(-1))
+        return out_vocab,hidden
+
+    def init_hidden(self,bsz,requires_grad=True):
+        weight = next(self.parameters())
+        return weight.new_zeros((1,bsz,self.hidden_size),requires_grad=True),weight.new_zeros((1, bsz, self.hidden_size), requires_grad=True)
+
+model = RNNModel(vocab_size=len(TEXT.vocab),
+                 embed_size=EMBEDDING_SIZE,
+                 hidden_size=HIDDEN_SIZE
+                 )
+
+def repackage_hidden(h):
+    if isinstance(h,torch.Tensor):
+        return h.detach()
+    else:
+        return tuple(repackage_hidden(v) for v in h )
+
+loss_fn = nn.CrossEntropyLoss()
+learning_rate = 0.001
+optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
+
+VOCAB_SIZE = len(TEXT.vocab)
+
+NUM_EPOCH = 2
+GRAD_CLIP = 5.0
+for epoch in range(NUM_EPOCH):
+    model.train()
+    it = iter(train_iter)
+    hidden = model.init_hidden(BATCH_SIZE)
+    for i , batch in enumerate(it):
+        data,target = batch.text,batch.target
+        hidden = repackage_hidden(hidden)
+        output,hidden = model(data,hidden)
+
+        loss = loss_fn(output.view(-1,VOCAB_SIZE),target.view(-1))
+        optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), GRAD_CLIP)
+        optimizer.step()
+        if i % 100 == 0:
+            print("loss",loss.item())
+
+        if i % 10000:
+            torch.save(model.state_dict(),"lm.pth")
